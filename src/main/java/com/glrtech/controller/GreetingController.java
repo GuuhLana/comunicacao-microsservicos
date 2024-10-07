940f504d1a966a95963658c5797c8bd8937a34d7
Gustavo Rocha <gustavolr401@gmail.com>
2024-10-06 23:58:21 -0300

    Adding Config Server

--- src/main/java/com/glrtech/controller/GreetingController.java
+++ src/main/java/com/glrtech/controller/GreetingController.java
@@ -1,30 +1,35 @@
-package com.glrtech;
+package com.glrtech.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.glrtech.config.GreetingConfiguration;
+import com.glrtech.model.Greeting;
+
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "%s, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	//@Autowired
-	//private GreetingConfiguration configuration;
+	@Autowired
+	private GreetingConfiguration configuration;
 	
 	@RequestMapping("/greeting")
 	public Greeting greeting(
 			@RequestParam(value="name",
-			defaultValue = "World") String name) {
+			defaultValue = "") String name) {
 		
-		//if (name.isEmpty()) name = configuration.getDefaultValue();
+		if (name.isEmpty()) name = configuration.getDefaultValue();
 		
 		return new Greeting(
 					counter.incrementAndGet(),
-					String.format(template, name)
+					String.format(template, configuration.getGreeting(), name)
 				);
 	}
 }
+
--- src/main/java/com/glrtech/model/Greeting.java
+++ src/main/java/com/glrtech/model/Greeting.java
@@ -1,4 +1,4 @@
-package com.glrtech;
+package com.glrtech.model;
 
 public class Greeting {
 	
